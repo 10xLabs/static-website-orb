9e0325cda86546198a61f6cd820f1ea6ee67310f
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-05-31 13:45:11 -0500

    Fix references

--- src/scripts/copy_files.sh
+++ src/scripts/copy_files.sh
@@ -1,7 +1,6 @@
-aws s3 sync $SYNC_OPTIONS --exclude logos/* --exclude sitemap.xml --cache-control max-age=31536000 --content-encoding gzip ./public s3://$BUCKET_NAME/
-aws s3 cp ./public/service-worker.js s3://$BUCKET_NAME/service-worker.js --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-encoding gzip --content-type application/javascript
-aws s3 cp ./public/index.html s3://$BUCKET_NAME/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-encoding gzip --content-type text/html
-aws s3 cp ./public/robots.txt s3://$BUCKET_NAME/robots.txt --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/plain
-aws s3 cp ./public s3://$BUCKET_NAME/ --metadata-directive REPLACE --cache-control max-age=31536000 --content-type application/json --recursive  --exclude '*' --include 'manifest.*'
-aws s3 cp ./public/icons/ s3://$BUCKET_NAME/icons/ --recursive --metadata-directive REPLACE --cache-control max-age=31536000 --content-type image/png
- 
+aws s3 sync "$SYNC_OPTIONS" --exclude logos/* --exclude sitemap.xml --cache-control max-age=31536000 --content-encoding gzip $SOURCE_FOLDER "s3://$BUCKET_NAME/"
+aws s3 cp "$SOURCE_FOLDER/service-worker.js" "s3://$BUCKET_NAME/service-worker.js" --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-encoding gzip --content-type application/javascript
+aws s3 cp "$SOURCE_FOLDER/index.html" "s3://$BUCKET_NAME/index.html" --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-encoding gzip --content-type text/html
+aws s3 cp "$SOURCE_FOLDER/robots.txt" "s3://$BUCKET_NAME/robots.txt" --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/plain
+aws s3 cp "$SOURCE_FOLDER" "s3://$BUCKET_NAME/" --metadata-directive REPLACE --cache-control max-age=31536000 --content-type application/json --recursive  --exclude '*' --include 'manifest.*'
+aws s3 cp "$SOURCE_FOLDER/icons/" "s3://$BUCKET_NAME/icons/" --recursive --metadata-directive REPLACE --cache-control max-age=31536000 --content-type image/png
