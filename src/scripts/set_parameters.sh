9cfd6c19dfad9b50d22feb31def1013da74e721c
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-06-16 14:23:56 -0500

    test

--- src/scripts/set_parameters.sh
+++ src/scripts/set_parameters.sh
@@ -1,12 +1,12 @@
 value=$(aws ssm get-parameter --name "/$ENVIRONMENT/$CIRCLE_PROJECT_REPONAME/PARAMETERS" --region "$AWS_DEFAULT_REGION" --with-decryption | jq '.Parameter.Value' | tr -d \")
 echo "$value"
 IFS=$'|' read -rd '' -a values <<<"$value"
+echo "$values"
 for val in "${values[@]}"
 do
     echo "**************************"
     echo "$val"
     echo "**************************"
-    echo "export $val" >> "$BASH_ENV"
+    # echo "export $val" >> "$BASH_ENV"
 done
-
-cat "$BASH_ENV"
+# cat "$BASH_ENV"
