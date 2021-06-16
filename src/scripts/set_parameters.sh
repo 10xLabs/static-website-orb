77bf3f6ab4de26adcc133459129cf2f629d78a81
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-06-16 11:15:36 -0500

    add set parameters

---
+++ src/scripts/set_parameters.sh
@@ -0,0 +1,8 @@
+value=$(aws-vault exec kolors-prod -- aws ssm get-parameter --name "/$ENVIRONMENT/$CIRCLE_PROJECT_REPONAME/PARAMETERS" | jq '.Parameter.Value' | tr -d \")
+readarray -t values <<<"$value"
+
+for val in "${values[@]}"
+do
+    echo "$val"
+	echo "export $val" >> "$BASH_ENV"
+done
