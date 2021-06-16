c5840229234aff117e7fc992dcb1e7f370347263
jbpineiroc <jbpineiroc@gmail.com>
2021-06-16 14:50:24 -0500

    [semver:minor] Set parameters for build 2.1.0 (#4)

---
+++ src/scripts/set_parameters.sh
@@ -0,0 +1,6 @@
+value=$(aws ssm get-parameter --name "/$ENVIRONMENT/$CIRCLE_PROJECT_REPONAME/PARAMETERS" --region "$AWS_DEFAULT_REGION" --with-decryption | jq '.Parameter.Value' | tr -d \")
+IFS="|" read -r -a values <<< "$value"
+for val in "${values[@]}"
+do
+    echo "export $val" >> "$BASH_ENV"
+done
