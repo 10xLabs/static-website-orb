92631fe43c38eeb2c15ef296548d7dea612c4348
jbpineiroc <jbpineiroc@gmail.com>
2021-06-21 10:28:33 -0500

    [semver:minor] Change Pulumi folder 2.2.0

--- src/scripts/set_parameters.sh
+++ src/scripts/set_parameters.sh
@@ -1,4 +1,4 @@
-value=$(aws ssm get-parameter --name "/$ENVIRONMENT/$CIRCLE_PROJECT_REPONAME/PARAMETERS" --region "$AWS_DEFAULT_REGION" --with-decryption | jq '.Parameter.Value' | tr -d \")
+value=$(aws ssm get-parameter --name "/$CIRCLE_PROJECT_REPONAME/PARAMETERS" --region "$AWS_DEFAULT_REGION" --with-decryption | jq '.Parameter.Value' | tr -d \")
 IFS="|" read -r -a values <<< "$value"
 for val in "${values[@]}"
 do
