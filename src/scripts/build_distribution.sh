81381da316b831a0fed6ef29f1d00b4407492e59
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-06-30 12:00:55 -0500

    add release parameter

--- src/scripts/build_distribution.sh
+++ src/scripts/build_distribution.sh
@@ -1,1 +1,7 @@
-npm run build
+RELEASE=$(echo "$CIRCLE_SHA1" | cut -c -7)
+COMMIT_MESSAGE=$(git log --format=%B -n 1 "$CIRCLE_SHA1")
+if [[ "$COMMIT_MESSAGE" =~ ^release[[:space:]]v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+$ ]]; then
+    RELEASE="${COMMIT_MESSAGE:9}"
+fi
+
+RELEASE="$RELEASE" npm run build
