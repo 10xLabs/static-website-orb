e567d906b1bc9d6cfffdb294710428094f80ebc8
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-07-01 10:48:31 -0500

    fix uppercase commit message start

--- src/scripts/build_distribution.sh
+++ src/scripts/build_distribution.sh
@@ -1,6 +1,6 @@
 RELEASE=$(echo "$CIRCLE_SHA1" | cut -c -7)
 COMMIT_MESSAGE=$(git log --format=%B -n 1 "$CIRCLE_SHA1")
-if [[ "$COMMIT_MESSAGE" =~ ^release[[:space:]]v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+$ ]]; then
+if [[ "$COMMIT_MESSAGE" =~ ^Release[[:space:]]v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+$ ]]; then
     RELEASE="${COMMIT_MESSAGE:9}"
 fi
 
