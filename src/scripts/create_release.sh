421486296839ef94bc79d4187b25345b70d719b9
blablo <blablo@gmail.com>
2022-08-31 11:56:34 -0400

    chore(deps): update circle ci config

--- src/scripts/create_release.sh
+++ src/scripts/create_release.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 COMMIT_BODY=$(git log -1 origin/master --pretty=format:%b)
 gh release create "$RELEASE_TAG" --notes "$COMMIT_BODY"
--- src/scripts/set_stack_name.sh
+++ src/scripts/set_stack_name.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 echo "export STACK_NAME=$CIRCLE_PROJECT_REPONAME.$ENVIRONMENT" >> "$BASH_ENV"
